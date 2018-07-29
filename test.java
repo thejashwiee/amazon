24de7b8fae4d7f252ad465faee89f46336d1a5d5
teja <thejashwieed94@gmail.com>
2018-07-29 19:28:06 +0530

    file changed in teju branch

--- test.java
+++ test.java
@@ -3,5 +3,8 @@
  system.out.println("hello world")
  system.out.println("i'm tejaswi")	 
  system.out.println("welcome to bangolore")
+<<<<<<< Updated upstream
 iam changing something
+=======
+>>>>>>> Stashed changes
  }
